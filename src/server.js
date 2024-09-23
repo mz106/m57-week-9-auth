443be628eccc6a433d8e14eb374d871b40a5e538
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-23 15:29:30 +0100

    enhancement: addUser route working

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ const express = require("express");
 
 const User = require("./users/model");
 
+const userRouter = require("./users/routes");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(userRouter);
 
 const syncTables = () => {
   User.sync();
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,4 +1,8 @@
 const { Router } = require("express");
 const userRouter = Router();
 
+const { addUser } = require("./controllers");
+
+userRouter.post("/users/signup", addUser);
+
 module.exports = userRouter;
